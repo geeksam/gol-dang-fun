b58a5a0e23d5c5829593bcf049589737a2906280
Sam Livingston-Gray <geeksam@gmail.com>
2021-09-16 16:07:41 -0700

    how many tests does it take to get to the center of a Tootsie Pop?

--- spec/conway_spec.rb
+++ spec/conway_spec.rb
@@ -11,14 +11,26 @@ RSpec.describe "the game of life (don't talk to me about life)" do
     verify_board { |x,y| (1..2).cover?(x) && (1..2).cover?(y) }
   end
 
+  specify "a square blinker cycles" do
+    verify_board(iterations: 4) { |x,y|
+      (
+        ( (0..1).cover?(x) && (0..1).cover?(y) ) ||
+        ( (2..3).cover?(x) && (2..3).cover?(y) )
+      )
+    }
+  end
 
 
-  def verify_board(&board)
+
+  def verify_board(iterations: 2, &board)
     verify do
       frames = []
       frames << GOL.print_grid(5, 4) { |x,y| board.(x,y) ? "x" : "." }
-      board = GOL.advance_turn(board)
-      frames << GOL.print_grid(5, 4) { |x,y| board.(x,y) ? "x" : "." }
+      # start at 2 because we had to seed the first one?
+      (2..iterations).each do
+        board = GOL.advance_turn(board)
+        frames << GOL.print_grid(5, 4) { |x,y| board.(x,y) ? "x" : "." }
+      end
       frames.join("\n--NEXT TURN--\n\n")
     end
   end
