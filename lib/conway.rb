d53d0ed92b56b9d07089902435471d4cedcae6ca
Sam Livingston-Gray <geeksam@gmail.com>
2021-09-16 16:34:26 -0700

    Naming Things

--- lib/conway.rb
+++ lib/conway.rb
@@ -1,27 +1,27 @@
 module GOL
   extend self
 
-  def print_grid(width, height, &board)
+  def print_grid(width, height, &fitness_test)
     t = ""
     (0..height).each do |y|
       (0..width).each do |x|
-        t << board.call(x,y).to_s
+        t << fitness_test.call(x,y).to_s
       end
       t << "\n"
     end
     t
   end
 
-  def advance_turn(board)
-    next_board = ->(x,y) {
+  def advance_turn(fitness_test)
+    next_fitness_test = ->(x,y) {
       living_neighbors = [
-        board.(x-1, y-1), board.(x, y-1), board.(x+1, y-1),
-        board.(x-1, y-0),                 board.(x+1, y-0),
-        board.(x-1, y+1), board.(x, y+1), board.(x+1, y+1),
+        fitness_test.(x-1, y-1), fitness_test.(x, y-1), fitness_test.(x+1, y-1),
+        fitness_test.(x-1, y-0),                        fitness_test.(x+1, y-0),
+        fitness_test.(x-1, y+1), fitness_test.(x, y+1), fitness_test.(x+1, y+1),
       ].map { |e| e ? 1 : 0 }.sum
-      living_neighbors == 3 || (living_neighbors == 2 && board.(x,y))
+      living_neighbors == 3 || (living_neighbors == 2 && fitness_test.(x,y))
     }
-    next_board
+    next_fitness_test
   end
 
 end
--- spec/conway_spec.rb
+++ spec/conway_spec.rb
@@ -2,17 +2,19 @@ require_relative 'spec_helper'
 require "approvals/rspec"
 
 RSpec.describe "the game of life (don't talk to me about life)" do
+  GRID_WIDTH  = 5
+  GRID_HEIGHT = 4
 
   specify "a single cell dies" do
-    verify_board { |x,y| x == 1 && y == 2 }
+    verify_fitness_test { |x,y| x == 1 && y == 2 }
   end
 
   specify "a square survives" do
-    verify_board { |x,y| (1..2).cover?(x) && (1..2).cover?(y) }
+    verify_fitness_test { |x,y| (1..2).cover?(x) && (1..2).cover?(y) }
   end
 
   specify "a square blinker cycles" do
-    verify_board(iterations: 4) { |x,y|
+    verify_fitness_test(iterations: 4) { |x,y|
       (
         ( (0..1).cover?(x) && (0..1).cover?(y) ) ||
         ( (2..3).cover?(x) && (2..3).cover?(y) )
@@ -21,22 +23,26 @@ RSpec.describe "the game of life (don't talk to me about life)" do
   end
 
   specify "a regular old blinker does whatever it does" do
-    verify_board(iterations: 4) { |x,y| (1..3).cover?(x) && y == 2 }
+    verify_fitness_test(iterations: 4) { |x,y| (1..3).cover?(x) && y == 2 }
   end
 
 
 
-  def verify_board(iterations: 2, &board)
+  def verify_fitness_test(iterations: 2, &fitness_test)
     verify do
-      frames = []
-      frames << GOL.print_grid(5, 4) { |x,y| board.(x,y) ? "x" : "." }
-      # start at 2 because we had to seed the first one?
-      (2..iterations).each do
-        board = GOL.advance_turn(board)
-        frames << GOL.print_grid(5, 4) { |x,y| board.(x,y) ? "x" : "." }
-      end
-      frames.join("\n--NEXT TURN--\n\n")
+      iterate_fitness_test(iterations, &fitness_test)
     end
+  end
+
+  def iterate_fitness_test(iterations=2, &fitness_test)
+    frames = []
+    frames << GOL.print_grid(5, 4) { |x,y| fitness_test.(x,y) ? "x" : "." }
+    # start at 2 because we had to seed the first one?
+    (2..iterations).each do
+      fitness_test = GOL.advance_turn(fitness_test)
+      frames << GOL.print_grid(5, 4) { |x,y| fitness_test.(x,y) ? "x" : "." }
+    end
+    frames.join("\n--NEXT TURN--\n\n")
   end
 
 end
